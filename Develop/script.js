b7a13765a3ed9a1d14c5287c79144a279838deca
Mariah <mariahjohnston217@outlook.com>
2022-06-02 19:55:03 -0400

    added moment library

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+//Steps:
+//1. Display current date in jumbotron.
+//2. Create timeblocks for each hour in a standard business day 9-5.
+//3. Color coordinate timeblocks for past, present and future.
+//4. Add click event so you can type directly into each box.
+//5. Create save button to save what was typed into textbox.
+//6. Save what was typed into the box into local storage.
+//7. Pull info from local storage and save in textbox.
+
+ var today = moment();
+ $("#currentDay").text(today.format("MMM Do, YYYY")); 
+
+
+
