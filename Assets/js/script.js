a3e5e2808116e128db96d6f27134f800aaf62f5c
Mariah <mariahjohnston217@outlook.com>
2022-06-02 22:42:28 -0400

    added text areas with hours 9am to 5pm, changed background color of textareas, added moment for current day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 //Steps:
-//1. Display current date in jumbotron.
+//1. Display current date in jumbotron. ✔️
 //2. Create timeblocks for each hour in a standard business day 9-5.
 //3. Color coordinate timeblocks for past, present and future.
 //4. Add click event so you can type directly into each box.
@@ -8,7 +8,7 @@
 //7. Pull info from local storage and save in textbox.
 
  var today = moment();
- $("#currentDay").text(today.format("MMM Do, YYYY")); 
+ $("#currentDay").text(today.format("dddd MMMM Do, YYYY")); 
 
 
 
