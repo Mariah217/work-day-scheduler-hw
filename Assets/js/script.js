02c315440dc3f48991c03ecd1c42450d87d84e66
Mariah <mariahjohnston217@outlook.com>
2022-06-03 20:54:59 -0400

    deleted input type from html, added save buttons for each text area

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,23 @@
 //Steps:
 //1. Display current date in jumbotron. ✔️
-//2. Create timeblocks for each hour in a standard business day 9-5.
+//2. Create timeblocks for each hour in a standard business day 9-5.✔️
 //3. Color coordinate timeblocks for past, present and future.
-//4. Add click event so you can type directly into each box.
-//5. Create save button to save what was typed into textbox.
+//4. Add click event so you can type directly into each box.✔️(used textarea)
+//5. Create save button to save what was typed into textbox next to each textbox.
 //6. Save what was typed into the box into local storage.
 //7. Pull info from local storage and save in textbox.
 
+
+//day of the week & date in jumbotron
  var today = moment();
  $("#currentDay").text(today.format("dddd MMMM Do, YYYY")); 
 
+//create time interval function for past, present & future
+
+//create functon for button???
+
+//create function for saving to local storage
+
+//create function to pull info from local storage & save in textbox
 
 
