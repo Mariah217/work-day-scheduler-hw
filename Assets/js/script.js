22f6748cb942cd622895946c2b43a6447d18fc53
Mariah <mariahjohnston217@outlook.com>
2022-06-03 23:15:59 -0400

    added var's for past present and future, added function for storing text area into local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,21 +3,32 @@
 //2. Create timeblocks for each hour in a standard business day 9-5.✔️
 //3. Color coordinate timeblocks for past, present and future.
 //4. Add click event so you can type directly into each box.✔️(used textarea)
-//5. Create save button to save what was typed into textbox next to each textbox.
+//5. Create save button to save what was typed into textbox next to each textbox.✔️
 //6. Save what was typed into the box into local storage.
 //7. Pull info from local storage and save in textbox.
 
 
 //day of the week & date in jumbotron
- var today = moment();
- $("#currentDay").text(today.format("dddd MMMM Do, YYYY")); 
+var today = moment();
+$("#currentDay").text(today.format("dddd MMMM Do, YYYY"));
 
-//create time interval function for past, present & future
+var currentTime = moment().format('H');
+console.log();
 
-//create functon for button???
+//variables:
+var pastHour = pastHour < currentTime;
+var presentHour = currentTime;
+var futureHour = futureHour > currentTime;
 
-//create function for saving to local storage
 
-//create function to pull info from local storage & save in textbox
+
+//save textarea to local storage
+function saveBtnClicked() {
+    document.getElementById("timeblock").value;
+    var timeBlock =document.getElementById("timeblock").value;
+    localStorage.setItem("planner", JSON.stringify(timeBlock));
+    console.log();
+}
+
 
 
